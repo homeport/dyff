d1262fe58861c00867f93ba8a5906c3d006d92ee
Matthias Diester <mddiester@gmail.com>
2018-08-12 00:06:59 +0200

    Fix download-latest.sh script
    Check if install location is writable before downloading dyff.
    
    Use user home directory bin directory as a fallback location in case
    it is on the path.
    
    Exit with non-zero exit code if no suitable install location can be found.

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -32,17 +32,31 @@ if ! hash jq 2>/dev/null; then
   exit 1
 fi
 
+# Find the latest dyff version using the GitHub API
 LATEST_TAG="$(curl --silent --location https://api.github.com/repos/HeavyWombat/dyff/releases | jq --raw-output '.[0].tag_name')"
 SYSTEM_UNAME="$(uname | tr '[:upper:]' '[:lower:]')"
-TARGET_FILE=/usr/local/bin/dyff
 
+# Find a suitable install location
+if touch /usr/local/bin/dyff 2>/dev/null; then
+  TARGET_FILE=/usr/local/bin/dyff
+
+elif grep -q -e "$HOME/bin" -e '\~/bin' <<<"$PATH"; then
+  TARGET_FILE=$HOME/bin/dyff
+
+else
+  echo "Unable to determine a writable install location. Make sure that you have write access to either /usr/local/bin or $HOME/bin."
+  exit 1
+fi
+
+# Download and install dyff
 case "${SYSTEM_UNAME}" in
   darwin | linux)
     DYFF_URI="https://github.com/HeavyWombat/dyff/releases/download/${LATEST_TAG}/dyff-${SYSTEM_UNAME}-amd64"
 
     echo -e "Downloading \\033[4;94m${DYFF_URI}\\033[0m to \\033[1m${TARGET_FILE}\\033[0m"
-    curl --progress-bar --location "${DYFF_URI}" --output "${TARGET_FILE}" && chmod a+rx "${TARGET_FILE}"
-    echo -e "\\nSuccessfully installed \\033[1m$(${TARGET_FILE} version)\\033[0m\\n"
+    if curl --progress-bar --location "${DYFF_URI}" --output "${TARGET_FILE}" && chmod a+rx "${TARGET_FILE}"; then
+      echo -e "\\nSuccessfully installed \\033[1m$(${TARGET_FILE} version)\\033[0m\\n"
+    fi
     ;;
 
   *)
