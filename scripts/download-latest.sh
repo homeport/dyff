5dc3c137f923164b2df27c49417de65a63aee238
Matthias Diester <matthias.diester@de.ibm.com>
2021-02-15 18:06:14 +0100

    Rewrite download script to be generic
    Add support for both tarball and binary download.
    
    Make script configurable through header environment variables.

--- scripts/download-latest.sh
+++ scripts/download-latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright © 2019 The Homeport Team
+# Copyright © 2021 The Homeport Team
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,57 +22,60 @@
 
 set -euo pipefail
 
+ORG=homeport
+REPO=dyff
+
 if ! hash curl 2>/dev/null; then
   echo "Required tool curl is not installed."
   exit 1
 fi
 
-if [[ "$(uname -m)" != "x86_64" ]]; then
-  echo -e "Unsupported machine type \\033[1m$(uname -m)\\033[0m: Please check \\033[4;94mhttps://api.github.com/repos/homeport/dyff/releases\\033[0m manually"
+if ! hash jq 2>/dev/null; then
+  echo -e 'Required tool \033[1mjq\033[0m is not installed.'
   exit 1
 fi
 
 if [[ $# -eq 0 ]]; then
-  if ! hash jq 2>/dev/null; then
-    echo -e 'Required tool \033[1mjq\033[0m is not installed.'
-    exit 1
-  fi
-
   # Find the latest version using the GitHub API
-  SELECTED_TAG="$(curl --silent --location https://api.github.com/repos/homeport/dyff/releases | jq --raw-output '.[0].tag_name')"
+  SELECTED_TAG="$(curl --silent --location https://api.github.com/repos/${ORG}/${REPO}/releases | jq --raw-output '.[0].tag_name')"
 else
   # Use provided argument as tag to download
   SELECTED_TAG="$1"
 fi
 
-SYSTEM_UNAME="$(uname | tr '[:upper:]' '[:lower:]')"
-
 # Find a suitable install location
 if [[ -w /usr/local/bin ]]; then
   TARGET_DIR=/usr/local/bin
 
-elif [[ -w "$HOME/bin" ]] && grep -q -e "$HOME/bin" -e '\~/bin' <<<"$PATH"; then
-  TARGET_DIR=$HOME/bin
+elif [[ -w "${HOME}/bin" ]] && grep -q -e "${HOME}/bin" -e '\~/bin' <<<"$PATH"; then
+  TARGET_DIR=${HOME}/bin
 
 else
-  echo -e "Unable to determine a writable install location. Make sure that you have write access to either \\033[1m/usr/local/bin\\033[0m or \\033[1m$HOME/bin\\033[0m and that is in your PATH."
+  echo -e "Unable to determine a writable install location. Make sure that you have write access to either \\033[1m/usr/local/bin\\033[0m or \\033[1m${HOME}/bin\\033[0m and that is in your PATH."
   exit 1
 fi
 
-# Download and install
-case "${SYSTEM_UNAME}" in
-  darwin | linux)
-    DOWNLOAD_URI="https://github.com/homeport/dyff/releases/download/${SELECTED_TAG}/dyff-${SYSTEM_UNAME}-amd64"
+SYSTEM_UNAME="$(uname | tr '[:upper:]' '[:lower:]')"
+SYSTEM_ARCH="$(uname -m | sed 's/x86_64/amd64/')"
 
-    echo -e "Downloading \\033[4;94m${DOWNLOAD_URI}\\033[0m to place it into \\033[1m${TARGET_DIR}\\033[0m"
-    if curl --progress-bar --location "${DOWNLOAD_URI}" --output "${TARGET_DIR}/dyff"; then
-      chmod a+rx "${TARGET_DIR}/dyff"
-      echo -e "\\nSuccessfully installed \\033[1mdyff\\033[0m into \\033[1m${TARGET_DIR}\\033[0m\\n"
-    fi
+# Download and install
+DOWNLOAD_URI="$(curl --silent --location "https://api.github.com/repos/${ORG}/${REPO}/releases/tags/${SELECTED_TAG}" | jq --raw-output ".assets[] | select( (.name | contains(\"${SYSTEM_UNAME}\")) and (.name | contains(\"${SYSTEM_ARCH}\")) ) | .browser_download_url")"
+if [[ -z ${DOWNLOAD_URI} ]]; then
+  echo -e "Unsupported operating system \\033[1m${SYSTEM_UNAME}\\033[0m or machine type \\033[1m${SYSTEM_ARCH}\\033[0m: Please check \\033[4;94mhttps://github.com/${ORG}/${REPO}/releases\\033[0m manually."
+  exit 1
+fi
+
+echo -e "Downloading \\033[4;94m${DOWNLOAD_URI}\\033[0m to install \\033[1m${TARGET_DIR}/${REPO}\\033[0m"
+case "${DOWNLOAD_URI}" in
+  *tar.gz)
+    curl --progress-bar --location "${DOWNLOAD_URI}" | tar -xzf - -C "${TARGET_DIR}" "${REPO}"
     ;;
 
   *)
-    echo "Unsupported operating system: ${SYSTEM_UNAME}"
-    exit 1
+    if curl --progress-bar --location "${DOWNLOAD_URI}" --output "${TARGET_DIR}/${REPO}"; then
+      chmod a+rx "${TARGET_DIR}/${REPO}"
+    fi
     ;;
 esac
+
+echo -e "\\nSuccessfully installed \\033[1m${TARGET_DIR}/${REPO}\\033[0m\\n"
