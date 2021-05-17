54ecad4fbd5d647454c5182746114f6eb2539270
Baran Dalgic <baran.dalgic@posteo.net>
2021-05-18 00:54:28 +0200

    Reports can be filtered on set of paths
    Fixes #157
    
    User wants to focus on specific differences and therefore wants to filter on
    paths in the report. All the others should be omitted.
    
    Refactor `Filter` function to make use of Map for more efficient
    processing.
    
    Add flag for provinding filtering arguments.
    
    Add `reports.go` for methods of `Report` structure
    
    Signed-off-by: Baran Dalgic <baran.dalgic@posteo.net>

---
+++ pkg/dyff/reports.go
@@ -0,0 +1,30 @@
+package dyff
+
+import "github.com/gonvenience/ytbx"
+
+// Filter accepts YAML paths as input and returns a new report with differences for those paths only
+func (r Report) Filter(paths ...ytbx.Path) (result Report) {
+	if len(paths) == 0 {
+		return r
+	}
+
+	result = Report{
+		From: r.From,
+		To:   r.To,
+	}
+
+	pathsMap := make(map[string]struct{})
+
+	for _, path := range paths {
+		pathsMap[path.String()] = struct{}{}
+	}
+
+	for _, diff := range r.Diffs {
+		diffPathString := diff.Path.String()
+		if _, ok := pathsMap[diffPathString]; ok {
+			result.Diffs = append(result.Diffs, diff)
+		}
+	}
+
+	return result
+}
