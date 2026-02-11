36e770c78acb78ae5ad8d32b733718590af5b07e
peterdeme <demepeter93@gmail.com>
2026-02-11 14:06:28 +0100

    refactor: extract Colorizer interface for pluggable diff coloring
    Introduce an exported `Colorizer` interface that encapsulates all color
    operations used by the human-readable diff output. `HumanReport` gets a
    `Colorizer` field that defaults to `BuntColorizer` (preserving existing
    behavior) when nil.
    
    This allows downstream consumers to swap in alternative coloring backends
    (e.g. `mgutz/ansi`) without forking, by implementing the interface and
    setting `report.Colorizer`.
    
    Changes:
    - `colors.go`: define `Colorizer` interface (15 methods) and
      `BuntColorizer` as the default implementation
    - `output.go`: deleted, YAML color functions moved to `BuntColorizer`
    - `output_human.go`: thread colorizer through `HumanReport`, convert
      `highlightRemovals`, `highlightAdditions`, and
      `showWhitespaceCharacters` to receiver methods

--- pkg/dyff/colors.go
+++ pkg/dyff/colors.go
@@ -24,18 +24,42 @@ import (
 	"fmt"
 
 	"github.com/gonvenience/bunt"
+	"github.com/gonvenience/neat"
 	"github.com/lucasb-eyer/go-colorful"
 )
 
+// Colorizer is the interface for applying colors to diff output. Implement this
+// to replace the default bunt-based coloring with an alternative (e.g. ANSI).
+type Colorizer interface {
+	Green(text string) string
+	Greenf(format string, a ...interface{}) string
+	Red(text string) string
+	Redf(format string, a ...interface{}) string
+	Yellowf(format string, a ...interface{}) string
+	LightGreen(text string) string
+	LightRed(text string) string
+	DimGray(text string) string
+	Bold(text string) string
+	Italic(text string) string
+	BoldGreen(text string) string
+	BoldRed(text string) string
+	StylizeHeader(header string) string
+	YAMLInRedishColors(input interface{}, useIndentLines bool) (string, error)
+	YAMLInGreenishColors(input interface{}, useIndentLines bool) (string, error)
+}
+
+// BuntColorizer is the default Colorizer using bunt and go-colorful.
+type BuntColorizer struct{}
+
 var (
-	additionGreen      = color("#58BF38")
-	modificationYellow = color("#C7C43F")
-	removalRed         = color("#B9311B")
+	additionGreen      = hexColor("#58BF38")
+	modificationYellow = hexColor("#C7C43F")
+	removalRed         = hexColor("#B9311B")
 )
 
-func color(hex string) colorful.Color {
-	color, _ := colorful.Hex(hex)
-	return color
+func hexColor(hex string) colorful.Color {
+	c, _ := colorful.Hex(hex)
+	return c
 }
 
 func render(format string, a ...interface{}) string {
@@ -44,52 +68,6 @@ func render(format string, a ...interface{}) string {
 	}
 
 	return fmt.Sprintf(format, a...)
-}
-
-func green(text string) string {
-	return colored(additionGreen, text)
-}
-
-func greenf(format string, a ...interface{}) string {
-	return coloredf(additionGreen, format, a...)
-}
-
-func red(text string) string {
-	return colored(removalRed, text)
-}
-
-func redf(format string, a ...interface{}) string {
-	return coloredf(removalRed, format, a...)
-}
-
-func yellowf(format string, a ...interface{}) string {
-	return coloredf(modificationYellow, format, a...)
-}
-
-func lightgreen(text string) string {
-	return colored(bunt.LightGreen, text)
-}
-
-func lightred(text string) string {
-	return colored(bunt.LightSalmon, text)
-}
-
-func dimgray(text string) string {
-	return colored(bunt.DimGray, text)
-}
-
-func bold(text string) string {
-	return bunt.Style(text,
-		bunt.EachLine(),
-		bunt.Bold(),
-	)
-}
-
-func italic(text string) string {
-	return bunt.Style(text,
-		bunt.EachLine(),
-		bunt.Italic(),
-	)
 }
 
 func colored(color colorful.Color, text string) string {
@@ -104,4 +82,109 @@ func coloredf(color colorful.Color, format string, a ...interface{}) string {
 		bunt.EachLine(),
 		bunt.Foreground(color),
 	)
+}
+
+func defaultColorizer() Colorizer {
+	return &BuntColorizer{}
+}
+
+func (c *BuntColorizer) Green(text string) string {
+	return colored(additionGreen, text)
+}
+
+func (c *BuntColorizer) Greenf(format string, a ...interface{}) string {
+	return coloredf(additionGreen, format, a...)
+}
+
+func (c *BuntColorizer) Red(text string) string {
+	return colored(removalRed, text)
+}
+
+func (c *BuntColorizer) Redf(format string, a ...interface{}) string {
+	return coloredf(removalRed, format, a...)
+}
+
+func (c *BuntColorizer) Yellowf(format string, a ...interface{}) string {
+	return coloredf(modificationYellow, format, a...)
+}
+
+func (c *BuntColorizer) LightGreen(text string) string {
+	return colored(bunt.LightGreen, text)
+}
+
+func (c *BuntColorizer) LightRed(text string) string {
+	return colored(bunt.LightSalmon, text)
+}
+
+func (c *BuntColorizer) DimGray(text string) string {
+	return colored(bunt.DimGray, text)
+}
+
+func (c *BuntColorizer) Bold(text string) string {
+	return bunt.Style(text,
+		bunt.EachLine(),
+		bunt.Bold(),
+	)
+}
+
+func (c *BuntColorizer) Italic(text string) string {
+	return bunt.Style(text,
+		bunt.EachLine(),
+		bunt.Italic(),
+	)
+}
+
+func (c *BuntColorizer) BoldGreen(text string) string {
+	return c.Bold(c.Green(text))
+}
+
+func (c *BuntColorizer) BoldRed(text string) string {
+	return c.Bold(c.Red(text))
+}
+
+func (c *BuntColorizer) StylizeHeader(header string) string {
+	return bunt.Style(
+		header,
+		bunt.ForegroundFunc(func(x int, _ int, _ rune) *colorful.Color {
+			switch {
+			case x < 7:
+				return &colorful.Color{R: .45, G: .71, B: .30}
+			case x < 13:
+				return &colorful.Color{R: .79, G: .76, B: .38}
+			case x < 21:
+				return &colorful.Color{R: .65, G: .17, B: .17}
+			}
+			return nil
+		}),
+	)
+}
+
+func (c *BuntColorizer) YAMLInRedishColors(input interface{}, useIndentLines bool) (string, error) {
+	return neat.NewOutputProcessor(useIndentLines, true, &map[string]colorful.Color{
+		"keyColor":           bunt.FireBrick,
+		"indentLineColor":    {R: 0.2, G: 0, B: 0},
+		"scalarDefaultColor": bunt.LightCoral,
+		"boolColor":          bunt.LightCoral,
+		"floatColor":         bunt.LightCoral,
+		"intColor":           bunt.LightCoral,
+		"multiLineTextColor": bunt.DarkSalmon,
+		"nullColor":          bunt.Salmon,
+		"emptyStructures":    bunt.LightSalmon,
+		"dashColor":          bunt.FireBrick,
+	}).ToYAML(input)
+}
+
+func (c *BuntColorizer) YAMLInGreenishColors(input interface{}, useIndentLines bool) (string, error) {
+	return neat.NewOutputProcessor(useIndentLines, true, &map[string]colorful.Color{
+		"keyColor":           bunt.Green,
+		"indentLineColor":    {R: 0, G: 0.2, B: 0},
+		"scalarDefaultColor": bunt.LimeGreen,
+		"boolColor":          bunt.LimeGreen,
+		"floatColor":         bunt.LimeGreen,
+		"intColor":           bunt.LimeGreen,
+		"multiLineTextColor": bunt.OliveDrab,
+		"nullColor":          bunt.Olive,
+		"emptyStructures":    bunt.DarkOliveGreen,
+		"dashColor":          bunt.Green,
+	}).ToYAML(input)
 }
